2c63eb896c18ca9a3111dcab52be06c0a2eb0b56
ayush1240000 <ayushsurve1240@gmail.com>
2024-05-04 16:15:39 +0530

    third commit

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,4 +1,25 @@
-export default function Home() {
-    return (<div>Hey I am home page</div>)
-  }
-  
+import React from 'react';
+function Home() {
+  return (
+    <>
+      <div 
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh', 
+        textAlign: 'center'
+      }}
+    >
+    <div className="home">
+      <header>
+        <h1>Welcome to Our Website!</h1>
+      </header>
+    </div>
+
+    </div>
+    </>
+  );
+}
+
+export default Home;
